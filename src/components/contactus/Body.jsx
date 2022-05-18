6731ab785151d05e36ab461d61a1b971f6b69126
mustafa jawadwala <mustafajw07@gmail.com>
2022-05-18 19:07:22 +0530

    products

--- src/components/contactus/Body.jsx
+++ src/components/contactus/Body.jsx
@@ -1,8 +1,7 @@
-import React from "react";
-
 function Body() {
-return (<>
-<div className="d-flex py-5 my-5">
+return (
+<>
+<div className="py-5 my-5 background">
   <div className="container" id="container-cu">
     <div className="screen">
       <div className="screen-body">
@@ -36,6 +35,43 @@ return (<>
     </div>
   </div>
 </div>
+{/* <div id="form" class="container">
+	<div class="row">
+			<h1 className="text-center">Get in touch</h1>
+	</div>
+
+	<div class="row input-container">
+			<div class="col-xs-12">
+				<div class="styled-input wide">
+					<input type="text" required />
+					<label>Name</label> 
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-12">
+				<div class="styled-input">
+					<input type="text" required />
+					<label>Email</label> 
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-12">
+				<div class="styled-input" style={{float:"right"}}>
+					<input type="text" required />
+					<label>Phone Number</label> 
+				</div>
+			</div>
+			<div class="col-xs-12">
+				<div class="styled-input wide">
+					<textarea required></textarea>
+					<label>Message</label>
+				</div>
+			</div>
+			<div className="form-group">
+        <div className="wrap">
+          <button className="button">Submit</button>
+        </div>
+      </div>
+	</div>
+</div>	 */}
 </>
 );
 }
